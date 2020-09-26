96761926d48537eab34bb7166ed82d9612bcdeaf
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-09-26 13:00:56 +0530

    For Loop Demo

---
+++ forLoopOneDemo.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# int; con; steps
+for((counter=1; counter<=5; counter=counter+1))
+do
+	echo  "Number: $counter"
+done
---
+++ forLoopTwoDemo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 1 2 3 6 8
+do
+	echo "Number: $i"
+done
